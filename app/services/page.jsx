ac86ce4130b089d36059ae13a885e610629409ee
gokrusk <nigelljama@gmail.com>
2023-04-23 00:26:52 -0500

    feat: :sparkles: services page

---
+++ app/services/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ServicesPage() {
+  return (
+    <div>
+      <title>Services</title>
+        <h1>Services</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti maxime rerum, asperiores assumenda blanditiis possimus. Facere, at nesciunt voluptate quidem optio doloribus. Ex esse dignissimos earum sapiente ad. Consequuntur, officiis?</p>
+    </div>
+  )
+}
