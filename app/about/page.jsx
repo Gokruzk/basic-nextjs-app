8e3a3a341aa86095ba779fa9d1e0b897764b7c86
gokrusk <nigelljama@gmail.com>
2023-04-23 00:26:30 -0500

    feat: :sparkles: about page

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AboutPage() {
+  return (
+    <div>
+      <title>About</title>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex sit sed sequi magnam. At, veniam aliquam? Odio aspernatur vel, accusantium molestias tenetur omnis harum expedita sequi animi doloribus reiciendis quis.</p>
+    </div>
+  )
+}
